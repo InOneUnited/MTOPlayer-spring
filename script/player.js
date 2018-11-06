4c88d0ca8a6d590d271f06da2bbe70723c6d678a
KrzysiekJodlowski <krzysztof.s.jodlowski@gmail.com>
2018-11-06 21:57:12 +0100

    Begin to integrate player bar with audio element

--- script/player.js
+++ script/player.js
@@ -1,5 +1,5 @@
 var Dropbox = Dropbox.Dropbox;
-/*
+
 function initDropbox() {
 
     let dropbox = new Dropbox({ accessToken: 'ZP2-tmvGcPAAAAAAAAAACR6nmTFyT8--8lLvhiLR_YL29SCy1ES4HSMNP69cu77d' });
@@ -42,4 +42,99 @@ function downloadSongs(dropbox) {
 }
 
 downloadSongs(initDropbox());
-*/
+
+
+
+// PASTED AND INITIALLY CUSTOMIZED FROM 
+// https://gist.githubusercontent.com/lukewduncan/3e041e4b22a50855f9faaf01dec40137/raw/b03fff5a96584e69ba1bf9e25eafc1ea20b58c09/audio-player.js
+
+
+function initProgressBar() {
+    var player = document.getElementById('player');
+    var length = player.duration
+    var current_time = player.currentTime;
+
+    // calculate total length of value
+    var totalLength = calculateTotalValue(length)
+    document.getElementById("end-time").innerHTML = totalLength;
+
+    // calculate current value time
+    var currentTime = calculateCurrentValue(current_time);
+    document.getElementById("start-time").innerHTML = currentTime;
+
+    var progressbar = document.getElementById('seek-object-container');
+    progressbar.value = (player.currentTime / player.duration);
+    progressbar.addEventListener("click", seek);
+
+    if (player.currentTime == player.duration) {
+        document.getElementById('play-song').className = "";
+    }
+
+    function seek(event) {
+        var percent = event.offsetX / this.offsetWidth;
+        player.currentTime = percent * player.duration;
+        progressbar.value = percent / 100;
+    }
+};
+
+function initPlayers(num) {     // PROBABLY 1 IS ALL WHAT WE WANT
+    // pass num in if there are multiple audio players e.g 'player' + i
+
+    for (var i = 0; i < num; i++) {
+        (function () {
+
+            // Variables
+            // ----------------------------------------------------------
+            // audio embed object
+            var playerContainer = document.getElementById('player-container'),
+                player = document.getElementById('player'),
+                isPlaying = false,
+                playBtn = document.getElementById('play-song');
+
+            // Controls Listeners
+            // ----------------------------------------------------------
+            if (playBtn != null) {
+                playBtn.addEventListener('click', function () {
+                    togglePlay()
+                });
+            }
+
+            // Controls & Sounds Methods
+            // ----------------------------------------------------------
+            function togglePlay() {
+                if (player.paused === false) {
+                    player.pause();
+                    isPlaying = false;
+                    document.getElementById('play-song').className = "";
+
+                } else {
+                    player.play();
+                    document.getElementById('play-song').className = "pause";
+                    isPlaying = true;
+                }
+            }
+        }());
+    }
+}
+
+function calculateTotalValue(length) {
+    var minutes = Math.floor(length / 60),
+        seconds_int = length - minutes * 60,
+        seconds_str = seconds_int.toString(),
+        seconds = seconds_str.substr(0, 2),
+        time = minutes + ':' + seconds
+
+    return time;
+}
+
+function calculateCurrentValue(currentTime) {
+    var current_hour = parseInt(currentTime / 3600) % 24,
+        current_minute = parseInt(currentTime / 60) % 60,
+        current_seconds_long = currentTime % 60,
+        current_seconds = current_seconds_long.toFixed(),
+        current_time = (current_minute < 10 ? "0" + current_minute : current_minute) + ":" + (current_seconds < 10 ? "0" + current_seconds : current_seconds);
+
+    return current_time;
+}
+
+initPlayers(document.getElementById('player-container').length);
