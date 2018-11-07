05516e5a1bd84c864d5a582a347e393776873772
KrzysztofP90 <krzysztof1.przybylowicz@gmail.com>
2018-11-07 13:31:23 +0100

    Create schema of login.

---
+++ script/index.js
@@ -0,0 +1,31 @@
+var Dropbox = Dropbox.Dropbox;
+
+
+function initListeners() {
+    document.getElementById("submit").addEventListener("click", submit)
+    
+}
+
+function submit() {
+    let login = document.getElementById("login").value;
+    let password = document.getElementById("password").value;
+
+    if (login == loginPattern && password == passwordPattern) {
+        var dbox = new Dropbox({ key: 'ZP2-tmvGcPAAAAAAAAAAD4J9jcQkIzzzb8q0QdCC5ssPk6RkSMQnA0t4CxzLXpqd' });
+        localStorage.setItem("key", dbox);
+        
+    }
+ 
+        
+    
+}
+ 
+
+var loginPattern = "j";  
+// "jodlaskrzychu";
+var passwordPattern = "j";
+// "jodla2018";
+
+var jsonKey = '{"key": "ZP2-tmvGcPAAAAAAAAAAD4J9jcQkIzzzb8q0QdCC5ssPk6RkSMQnA0t4CxzLXpqd"}';
+console.log("wchodzi");
+initListeners();
