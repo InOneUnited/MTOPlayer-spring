74d76c82c6a2236a356b977976d611841dd42728
KrzysiekJodlowski <krzysztof.s.jodlowski@gmail.com>
2018-11-07 21:43:56 +0100

    Make player bar switch to next and previous song working

--- script/player.js
+++ script/player.js
@@ -11,7 +11,6 @@ function initDropbox() {
         .catch(function (error) {
             console.log(error);
         });
-
     return dropbox;
 }
 
@@ -47,36 +46,27 @@ function downloadSong(dropbox, songList, number) {
 }
 
 
-let dropbox = initDropbox();
-let songList = ["/dee_yan-key_-_02_-_winter_is_coming_adagio_-_first_snow.mp3",
-    "/starfucker - bury us alive.mp3",
-    "/stoned jesus - indian.mp3"]
-
-// initialize with first song
-let currentSong = 0;
-downloadSong(dropbox, songList, currentSong);
-
-
 // PASTED AND INITIALLY CUSTOMIZED FROM 
 // https://gist.githubusercontent.com/lukewduncan/3e041e4b22a50855f9faaf01dec40137/raw/b03fff5a96584e69ba1bf9e25eafc1ea20b58c09/audio-player.js
 
 
 function initProgressBar() {
-    var player = document.getElementById('player');
-    var length = player.duration;
-    var current_time = player.currentTime;
+    let player = document.getElementById('player');
+    let length = player.duration;
+    let current_time = player.currentTime;
 
-    // calculate total time length
-    var totalLength = calculateTotalValue(length);
+    let totalLength = calculateTotalTime(length);
     document.getElementById("end-time").innerHTML = totalLength;
 
-    // calculate current value time
-    var currentTime = calculateCurrentValue(current_time);
+    let currentTime = calculateCurrentTime(current_time);
     document.getElementById("start-time").innerHTML = currentTime;
 
-    // progress bar
-    var progressbar = document.getElementById('seekBar');
-    progressbar.value = (player.currentTime / player.duration);
+    let progressbar = document.getElementById('seekBar');
+    if (isNaN(player.currentTime / player.duration)) {
+        progressbar.value = 0;
+    } else {
+        progressbar.value = (player.currentTime / player.duration);
+    }
     progressbar.addEventListener("click", seek);
 
     if (player.currentTime == player.duration) {
@@ -84,79 +74,77 @@ function initProgressBar() {
     }
 
     function seek(event) {
-        var percent = event.offsetX / this.offsetWidth;
+        let percent = event.offsetX / this.offsetWidth;
         player.currentTime = percent * player.duration;
         progressbar.value = percent / 100;
     }
 };
 
-function initPlayers() {
+function initPlayer(dropbox, songList, currentSong) {
 
-    // Variables
-    // ----------------------------------------------------------
-    // audio embed object
-    var playerContainer = document.getElementById('player-container');
-    var player = document.getElementById('player');
-    var isPlaying = false;
-    var playBtn = document.getElementById('play-song');
-    var pauseBtn = document.getElementById('pause-song');
-    var stopBtn = document.getElementById('stop-song');
-    var prevBtn = document.getElementById('prev-song');
-    var nextBtn = document.getElementById('next-song');
+    // var playerContainer = document.getElementById('player-container');
+    let player = document.getElementById('player');
+    // let isPlaying = false;
+    let playBtn = document.getElementById('play-song');
+    let pauseBtn = document.getElementById('pause-song');
+    let stopBtn = document.getElementById('stop-song');
+    let prevBtn = document.getElementById('prev-song');
+    let nextBtn = document.getElementById('next-song');
 
-    // Controls Listeners
-    // ----------------------------------------------------------
     playBtn.addEventListener('click', function () {
         togglePlayOn();
     });
+
     pauseBtn.addEventListener('click', function () {
         togglePlayOff();
     });
+
     stopBtn.addEventListener('click', function () {
         togglePlayOff();
         player.load();
 
         document.getElementById('seekBar').value = 0;
 
-        var length = document.getElementById('player').duration;
-        var totalLength = calculateTotalValue(length);
+        let length = document.getElementById('player').duration;
+        let totalLength = calculateTotalTime(length);
         document.getElementById("end-time").innerHTML = totalLength;
+        player.removeAttribute('autoplay');
     });
+
     prevBtn.addEventListener('click', function () {
 
+        document.getElementById('seekBar').value = 0;
+        togglePlayOff();
         if (currentSong === 0) {
-            currentSong = songList[songList.length - 1];
+            currentSong = songList.length - 1;
         } else {
             currentSong -= 1;
         }
-        togglePlayOff();
-        player.pause();
+        // isPlaying = false;
         downloadSong(dropbox, songList, currentSong);
-        initProgressBar();
-        player.play();
+        togglePlayOn();
+        player.setAttribute('autoplay', true);
     });
+
     nextBtn.addEventListener('click', function () {
 
+        document.getElementById('seekBar').value = 0;
+        togglePlayOff();
         if (currentSong === songList.length - 1) {
             currentSong = 0;
         } else {
             currentSong += 1;
         }
-        togglePlayOff();
-        player.pause();
-        document.getElementById('seekBar').value = "0";
+        // isPlaying = true;
         downloadSong(dropbox, songList, currentSong);
-        initProgressBar();
-        player.play();
+        togglePlayOn();
+        player.setAttribute('autoplay', true);
     });
 
-    // Controls & Sounds Methods
-    // ----------------------------------------------------------
     function togglePlayOn() {
 
         player.play();
-        isPlaying = true;
-
+        // isPlaying = true;
         document.getElementById('play-song').style.display = "none";
         document.getElementById('pause-song').style.display = "block";
     }
@@ -164,40 +152,55 @@ function initPlayers() {
     function togglePlayOff() {
 
         player.pause();
-        isPlaying = false;
-
+        // isPlaying = false;
         document.getElementById('play-song').style.display = "block";
         document.getElementById('pause-song').style.display = "none";
     }
 }
 
-function calculateTotalValue(length) {
+function calculateTotalTime(length) {
 
-    var minutes = Math.floor(length / 60);
-    var seconds_int = length - minutes * 60;
-    var seconds_str = seconds_int.toString();
-    var second = seconds_str.substr(0, 2);
+    let minutes = Math.floor(length / 60);
+    let seconds_int = length - minutes * 60;
+    let seconds_str = seconds_int.toString();
+    let second = seconds_str.substr(0, 2);
 
     if (isNaN(minutes)) {
         minutes = "00";
     }
-    if (second === "Na") {
+    if ((second === "Na") || (second === "0.")) {
         second = "00";
     }
-
-    var time = minutes + ':' + second;
-
-    return time;
+    return minutes + ':' + second;
 }
 
-function calculateCurrentValue(currentTime) {
-    var current_minute = parseInt(currentTime / 60) % 60;
-    var current_seconds_long = currentTime % 60;
-    var current_seconds = current_seconds_long.toFixed();
-    var current_time = (current_minute < 10 ? "0" + current_minute : current_minute) +
+function calculateCurrentTime(currentTime) {
+    let current_minute = parseInt(currentTime / 60) % 60;
+    let current_seconds_long = currentTime % 60;
+    let current_seconds = current_seconds_long.toFixed();
+    let current_time = (current_minute < 10 ? "0" + current_minute : current_minute) +
         ":" + (current_seconds < 10 ? "0" + current_seconds : current_seconds);
 
     return current_time;
 }
 
-initPlayers();
+function main() {
+
+    let dropbox = initDropbox();
+    let songList = ["/starfucker - bury us alive.mp3",
+        "/stoned jesus - indian.mp3",
+        "/dee_yan-key_-_02_-_winter_is_coming_adagio_-_first_snow.mp3"]
+
+    // initialize with first song
+    let currentSong = 0;
+    downloadSong(dropbox, songList, currentSong);
+    initPlayer(dropbox, songList, currentSong);
+
+    document.getElementById('logout').addEventListener('click', function () {
+        dropbox = null;
+        document.location.href = "index.html"
+    }, false);
+}
+
+main();
+
