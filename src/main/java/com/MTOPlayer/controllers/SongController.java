c15ef66c69fdc7d7933e945594acad8faa59a0a8
kaspazza <kaspazza@gmail.com>
2019-03-17 17:24:46 +0100

    simple get controller and page for song

--- src/main/java/com/MTOPlayer/controllers/SongController.java
+++ src/main/java/com/MTOPlayer/controllers/SongController.java
@@ -1,4 +1,14 @@
 package com.MTOPlayer.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
 public class SongController {
+    @RequestMapping(value = "/song/{songId}", method = RequestMethod.GET)
+    public String showSong(@PathVariable int songId){
+        return "player";
+    }
 }
