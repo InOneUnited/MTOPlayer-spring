594173ff4d582b1b01e57124a30393abd541a6f9
kaspazza <kaspazza@gmail.com>
2019-03-17 01:17:12 +0100

    create get handler and simple html/sass for login

--- src/main/java/com/MTOPlayer/controllers/SignInController.java
+++ src/main/java/com/MTOPlayer/controllers/SignInController.java
@@ -1,4 +1,14 @@
 package com.MTOPlayer.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
 public class SignInController {
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public String showLogin(Model model){
+        return "login";
+    }
 }
