7f4ab41dc274052cbef03a356d90dc00ec788774
kaspazza <kaspazza@gmail.com>
2019-03-24 18:50:27 +0100

    retrive salt by password id

--- src/main/java/com/MTOPlayer/dao/BasicSaltDAO.java
+++ src/main/java/com/MTOPlayer/dao/BasicSaltDAO.java
@@ -1,4 +1,22 @@
 package com.MTOPlayer.dao;
 
-public class BasicSaltDAO {
+import java.io.IOException;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class BasicSaltDAO extends DAO implements SaltDAO {
+    @Override
+    public byte[] getSalt(int passwordId) throws IOException, SQLException {
+        byte[] salt = new byte[0];
+
+        String query = "SELECT salt FROM salt WHERE password_id=\'" +passwordId+"\';";
+
+        ResultSet result = fillResult(query);
+
+
+        while(result.next()){
+            salt = result.getBytes(1);
+        }
+        return salt;
+    }
 }
--- src/main/java/com/MTOPlayer/dao/SaltDAO.java
+++ src/main/java/com/MTOPlayer/dao/SaltDAO.java
@@ -1,4 +1,8 @@
 package com.MTOPlayer.dao;
 
+import java.io.IOException;
+import java.sql.SQLException;
+
 public interface SaltDAO {
+    byte[] getSalt(int passwordId) throws IOException, SQLException;
 }
