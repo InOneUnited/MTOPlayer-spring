bbdee8a5cc346cb5d3ed38edc905c686fa538932
kaspazza <kaspazza@gmail.com>
2019-03-24 17:55:10 +0100

    create post mapping for login

--- src/main/java/com/MTOPlayer/controllers/SignInController.java
+++ src/main/java/com/MTOPlayer/controllers/SignInController.java
@@ -1,14 +1,35 @@
 package com.MTOPlayer.controllers;
 
+import com.MTOPlayer.models.TemporaryPassword;
+import com.MTOPlayer.models.User;
+import com.MTOPlayer.service.BasicLoginService;
+import com.MTOPlayer.service.LoginService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class SignInController {
-    @RequestMapping(value = "/login", method = RequestMethod.GET)
-    public String showLogin(Model model){
+    @GetMapping("/login")
+    public String showLogin(Model userModel, Model passwordModel){
+        userModel.addAttribute("user", new User());
+        passwordModel.addAttribute("temporaryPassword", new TemporaryPassword());
         return "login";
+    }
+
+    @PostMapping("/login")
+    public String processLogin(Model model, @ModelAttribute User user, @ModelAttribute TemporaryPassword password){
+        LoginService loginService = new BasicLoginService();
+        if(loginService.isEmailInDB(user.getEmail())){
+            if(loginService.isPasswordCorrect(user.getEmail(), password.getPassword())){
+                return "redirect:/library";
+            } else {
+                model.addAttribute("passwordNotCorrectError", "true");
+                return "login";
+            }
+        } else {
+            model.addAttribute("emailDoesNotExistsError", "true");
+            return "login";
+        }
     }
 }
