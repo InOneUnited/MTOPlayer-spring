569961a7b6bea1a860ec5c706f0ecb14bc93ca40
kaspazza <kaspazza@gmail.com>
2019-04-22 15:28:24 +0200

    create sideBar

--- src/main/java/com/MTOPlayer/controllers/ProfileController.java
+++ src/main/java/com/MTOPlayer/controllers/ProfileController.java
@@ -1,22 +1,30 @@
 package com.MTOPlayer.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
+@RequestMapping("/profile")
 public class ProfileController {
-    @RequestMapping(value = "/profile/info", method = RequestMethod.GET)
-    public String showUserInfo() {
-        return "userInfo";
+    @RequestMapping(value = "/info", method = RequestMethod.GET)
+    public String showUserInfo(Model model) {
+        String pageName = "Profile";
+        String songName = "hello";
+        String artistName = "world";
+        model.addAttribute("pageName", pageName);
+        model.addAttribute("songName", songName);
+        model.addAttribute("playerInformation", artistName);
+        return "sideBar";
     }
 
-    @RequestMapping(value = "/profile/password", method = RequestMethod.GET)
+    @RequestMapping(value = "/password", method = RequestMethod.GET)
     public String showPasswordChange() {
         return "changePassword";
     }
 
-    @RequestMapping(value = "/profile/apps", method = RequestMethod.GET)
+    @RequestMapping(value = "/apps", method = RequestMethod.GET)
     public String showConnectedApps() {
         return "connectedApps";
     }
