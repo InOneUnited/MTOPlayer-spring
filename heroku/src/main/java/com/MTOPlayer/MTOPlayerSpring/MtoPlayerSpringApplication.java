1777cafd896428cdc852765e4fd4fb29a630cdc3
KrzysiekJodlowski <krzysztof.s.jodlowski@gmail.com>
2019-07-20 18:12:55 +0200

    Initialize proper Spring Boot project

---
+++ heroku/src/main/java/com/MTOPlayer/MTOPlayerSpring/MtoPlayerSpringApplication.java
@@ -0,0 +1,13 @@
+package com.MTOPlayer.MTOPlayerSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MtoPlayerSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MtoPlayerSpringApplication.class, args);
+	}
+
+}
