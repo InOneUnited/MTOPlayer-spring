bf88e4e6308b1e85c2dac7d49d0c7a935c74a84f
KrzysiekJodlowski <krzysztof.s.jodlowski@gmail.com>
2019-07-20 18:32:53 +0200

    Remove old daos from repository

--- heroku/src/main/java/com/MTOPlayer/MTOPlayerSpring/service/LoginService.java
+++ heroku/src/main/java/com/MTOPlayer/MTOPlayerSpring/service/LoginService.java
@@ -1,15 +1,15 @@
-package com.MTOPlayer.MTOPlayerSpring.service;
-
-
-import com.MTOPlayer.models.User;
-import com.MTOPlayer.models.UserInfo;
-
-import java.io.IOException;
-import java.sql.SQLException;
-
-public interface LoginService {
-    boolean isUserNew(User user) throws SQLException, IOException;
-    void addNewUser(User user, UserInfo userInfo, String password) throws IOException, SQLException;
-    boolean isEmailInDB(String email) throws IOException, SQLException;
-    boolean isPasswordCorrect(String email, String password) throws IOException, SQLException;
-}
+//package com.MTOPlayer.MTOPlayerSpring.service;
+//
+//
+//import com.MTOPlayer.models.User;
+//import com.MTOPlayer.models.UserInfo;
+//
+//import java.io.IOException;
+//import java.sql.SQLException;
+//
+//public interface LoginService {
+//    boolean isUserNew(User user) throws SQLException, IOException;
+//    void addNewUser(User user, UserInfo userInfo, String password) throws IOException, SQLException;
+//    boolean isEmailInDB(String email) throws IOException, SQLException;
+//    boolean isPasswordCorrect(String email, String password) throws IOException, SQLException;
+//}
